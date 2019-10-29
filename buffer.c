281f68f720ac66845caa757f73dac6615f250187
Noah <1101qbtext@gmail.com>
2019-10-28 22:28:29 -0400

    Changed macros in buffer.c to be in line with the pascal naming convention.
    Replaced the if/if else statements with a big switch statement.
    
    No more Lexer type instead just increment the pointer to get the next value in the array.
    
    Added a rudimentary error reporting system.
    
    A few general small improvements.

--- buffer.c
+++ buffer.c
@@ -6,21 +6,26 @@ typedef struct
     int capacity;
 } BufferHeader;
 
-#define buffer_header_get(buffer) ((BufferHeader *)((char *)buffer - sizeof(BufferHeader)))
-#define buffer_get(buffer_header) ((void *)((char *)buffer_header + sizeof(BufferHeader)))
+#define BufferHeaderGet(buffer) ((BufferHeader *)((char *)buffer - sizeof(BufferHeader)))
+#define BufferGet(buffer_header) ((void *)((char *)buffer_header + sizeof(BufferHeader)))
 
-#define buffer_check_if_reallocation_is_needed(buffer, item_size) \
-if(buffer_length(buffer) >= buffer_capacity(buffer)) \
+#define BufferCheckIfReallocationIsNeeded(buffer, item_size) \
+if(BufferLength(buffer) >= BufferCapacity(buffer)) \
 { \
-    buffer_reallocate((void **)(&buffer), item_size, buffer ? buffer_capacity(buffer) * 2 : 32); \
+    BufferReallocate((void **)(&buffer), item_size, buffer ? BufferCapacity(buffer) * 2 : 32); \
 } 0\
 
-#define buffer_push(buffer, item) \
-buffer_check_if_reallocation_is_needed(buffer, sizeof item); \
-buffer[buffer_length(buffer)] = item; \
-buffer_header_get(buffer)->length++ \
+#define BufferPush(buffer, item) \
+BufferCheckIfReallocationIsNeeded(buffer, sizeof item); \
+buffer[BufferLength(buffer)] = item; \
+BufferHeaderGet(buffer)->length++ \
 
-void buffer_reallocate(void **buffer, int item_size, int new_capacity)
+#define BufferFree(buffer) free(BufferHeaderGet(buffer))
+
+#define BufferLength(buffer) (buffer ? BufferHeaderGet(buffer)->length : 0)
+#define BufferCapacity(buffer) (buffer ? BufferHeaderGet(buffer)->capacity : 0)
+
+void BufferReallocate(void **buffer, int item_size, int new_capacity)
 {
     if(!*buffer)
     {
@@ -28,16 +33,13 @@ void buffer_reallocate(void **buffer, int item_size, int new_capacity)
         header->length = 0;
         header->capacity = new_capacity;
         
-        *buffer = buffer_get(header);
+        *buffer = BufferGet(header);
     } else
     {
-        BufferHeader *header = buffer_header_get(*buffer);
+        BufferHeader *header = BufferHeaderGet(*buffer);
         header = realloc(header, sizeof *header + (item_size * new_capacity));
         header->capacity = new_capacity;
         
-        *buffer = buffer_get(header);
+        *buffer = BufferGet(header);
     }
 }
-
-#define buffer_length(buffer) (buffer ? buffer_header_get(buffer)->length : 0)
-#define buffer_capacity(buffer) (buffer ? buffer_header_get(buffer)->capacity : 0)
--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+/*
+Things to possibly purse:
+
+1. Should the stretchy buffer implementation offer easy iteration?
+
+2. Another we can iterate through an array is using pointers. Increment and then deference to get the next object.
+   No need to create a separate type for everything we need to loop over.
+
+3. Error reporting for sure.
+
+4. Complete the lexer.
+
+5. Allow a forcible shutdown of the compilation purely for testing purposes in the error reporting system?
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <ctype.h>
 
@@ -8,27 +25,6 @@
 { fprintf(stderr, "Assertion failed!\nCondtion: %s\nFile: %s\nLine:%d\n", #condition, __FILE__, __LINE__); } 0\
 
 #include "buffer.c"
-
-void BufferTest(void)
-{
-    int *numbers = NULL;
-    int max_number = 2048;
-    int i = 0;
-    
-    while(i < max_number)
-    {
-        buffer_push(numbers, i);
-        i++;
-    }
-    
-    i = 0;
-    while(i < max_number)
-    {
-        Assert(numbers[i] == i);
-        i++;
-    }
-    
-}
 
 typedef enum
 {
@@ -58,6 +54,8 @@ static char const *token_string_table[] = {
 typedef struct
 {
     TokenKind kind;
+    int line;
+    int column;
     
     union
     {
@@ -67,140 +65,216 @@ typedef struct
     };
 } Token;
 
-typedef struct
-{
-    char const *buffer;
-    int         current;
-} Lexer;
+static int errors_reported = 0;
+static int max_allowed_errors = 20;
 
-Lexer LexerCreateFromString(char const *string)
+void ReportError(char const *format, ...)
 {
-    Lexer lexer;
-    lexer.buffer = string;
-    lexer.current = 0;
-    
-    return lexer;
-}
-
-char LexerPeek(Lexer *lexer)
-{
-    return lexer->buffer[lexer->current];
-}
-
-char LexerNext(Lexer *lexer)
-{
-    char result = LexerPeek(lexer);
-    if(result != 0)
+    if(errors_reported >= max_allowed_errors)
     {
-        lexer->current++;
+        fprintf(stderr, "Too many errors!\nMax error limit is %d\n", max_allowed_errors);
+        exit(1);
     }
     
-    return result;
+    va_list argument_list;
+    va_start(argument_list, format);
+    vfprintf(stderr, format, argument_list);
+    va_end(argument_list);
+    
+    errors_reported++;
 }
 
-Token *LexerRun(Lexer *lexer)
+Token *LexerRun(char *lexer)
 {
+    int current_line = 1;
+    int current_column = 1;
     Token *list_of_tokens = NULL;
     Token current_token;
+    char *token_start = lexer;
+    char *token_end = token_start;
+    bool add_token = false;
     char c;
-    while((c = LexerPeek(lexer)) != 0)
+    while((c = *(token_start = lexer)) != 0)
     {
-        if(isspace(c))
+        switch(c)
         {
-            LexerNext(lexer);
-            continue;
-        } else if(isalpha(c) || c == '_')
-        {
-            int index = 0;
-            c = LexerNext(lexer);
-            while(isalnum(c) || c == '_')
+            case ' ':
+            case '\t':
+            case '\n':
+                lexer++;
+                
+                if(c == '\n')
+                {
+                    current_line++;
+                }
+            break;
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
             {
-                current_token.name[index++] = c;
-                c = LexerNext(lexer);
+            	int result = 0;
+            	while(isdigit(c))
+            	{
+                    result *= 10;
+                    result += c - '0';
+                    c = *++lexer;
+                    
+                    if((result * 0.5) >= (INT_MAX * 0.5))
+                    {
+                        ReportError("[%d : %d] Number literal surpasses what is allowed!\nMax allowed: %d\n", current_line, current_column, INT_MAX);
+                    }
+                }
+            
+                current_token.number = result;
+                current_token.kind = TOKEN_NUMBER;
+                add_token = true;
             }
+            break;
             
-            current_token.name[index] = 0;
-            current_token.kind = TOKEN_IDENTIFIER;
-        } else if(isdigit(c))
-        {
-            int result = 0;
-            while(isdigit(c = LexerNext(lexer)))
-            {
-                result *= 10;
-                result += c - '0';
-            }
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'u':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'y':
+            case 'z':
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
+            case 'G':
+            case 'H':
+            case 'I':
+            case 'J':
+            case 'K':
+            case 'L':
+            case 'M':
+            case 'N':
+            case 'O':
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+            case 'T':
+            case 'U':
+            case 'V':
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+            case '_':
             
-            current_token.number = result;
-            current_token.kind = TOKEN_NUMBER;
-        } else if(c == '"')
-        {
-            LexerNext(lexer);
-            char *start = (char *)&lexer->buffer[lexer->current];
-            char *end = start;
-            while((c = LexerNext(lexer)) != '"')
-            {
-                end++;
-            }
+            break;
             
-            int string_length = (int)(end - start);
-            current_token.string = malloc(string_length + 1);
-            strncpy(current_token.string, start, string_length);
+            case '"':
             
-            current_token.kind = TOKEN_STRING;
+            break;
+            
+            default:
+                ReportError("[%d : %d] Unrecognized character '%c'\n", current_line, current_column, c);
+            break;
         }
         
-        buffer_push(list_of_tokens, current_token);
+        token_end = lexer;
+        current_column = (int)(token_end - token_start);
+        
+        if(add_token)
+        {
+            current_token.line = current_line;
+            current_token.column = current_column;
+            BufferPush(list_of_tokens, current_token);
+            add_token = false;
+        }
     }
     
     Token eof_token;
     eof_token.kind = TOKEN_EOF;
-    
-    buffer_push(list_of_tokens, eof_token);
+    BufferPush(list_of_tokens, eof_token);
     
     return list_of_tokens;
 }
 
-#define TokenAssertIdentifier(token, string) Assert(strncmp(token.name, string, strlen(token.name)) == 0)
-#define TokenAssertNumber(token, value) Assert(token.number == value)
-#define TokenAssertString(token, string_value) Assert(strncmp(token.string, string_value, strlen(token.string)) == 0)
-#define TokenAssertKind(token, token_kind) Assert(token.kind == token_kind)
+#define TokenAssertIdentifier(tokens, string) \
+Assert(strncmp(tokens->name, string, strlen(tokens->name)) == 0); tokens++ \
+
+#define TokenAssertNumber(tokens, value) \
+Assert(tokens->number == value); tokens++\
+
+#define TokenAssertString(tokens, string_value) \
+Assert(strncmp(tokens->string, string_value, strlen(tokens->string)) == 0); tokens++ \
+
+#define TokenAssertKind(tokens, token_kind) \
+Assert(tokens->kind == token_kind); tokens++ \
 
 void LexerTest(void)
 {
-    Lexer lexer = LexerCreateFromString("test another_test");
-    Token *test_tokens = LexerRun(&lexer);
-    int token_index = 0;
+    Token *old_test_tokens_pointer;
+    Token *test_tokens = LexerRun("1 2 3 10 20 30");
+    old_test_tokens_pointer = test_tokens;
     
-    TokenAssertIdentifier(test_tokens[token_index], "test");
-    token_index++;
-    TokenAssertIdentifier(test_tokens[token_index], "another_test");
-    token_index++;
-    TokenAssertKind(test_tokens[token_index], TOKEN_EOF);
+    TokenAssertNumber(test_tokens, 1);
+    TokenAssertNumber(test_tokens, 2);
+    TokenAssertNumber(test_tokens, 3);
+    TokenAssertNumber(test_tokens, 10);
+    TokenAssertNumber(test_tokens, 20);
+    TokenAssertNumber(test_tokens, 30);
+    TokenAssertKind(test_tokens, TOKEN_EOF);
     
-    lexer = LexerCreateFromString("1 2 3");
-    test_tokens = LexerRun(&lexer);
-    token_index = 0;
-    
-    TokenAssertNumber(test_tokens[token_index], 1);
-    token_index++;
-    TokenAssertNumber(test_tokens[token_index], 2);
-    token_index++;
-    TokenAssertNumber(test_tokens[token_index], 3);
-    token_index++;
-    TokenAssertKind(test_tokens[token_index], TOKEN_EOF);
-    
-    lexer = LexerCreateFromString("\"test string\" \"another test string\"");
-    test_tokens = LexerRun(&lexer);
-    token_index = 0;
-    
-    TokenAssertString(test_tokens[token_index], "test string");
-    token_index++;
-    TokenAssertString(test_tokens[token_index], "another test string");
-    token_index++;
-    TokenAssertKind(test_tokens[token_index], TOKEN_EOF);
+    BufferFree(old_test_tokens_pointer);
 }
 
-int main(void)
+void BufferTest(void)
+{
+    int *numbers = NULL;
+    int max_number = 2048;
+    int i = 0;
+    
+    while(i < max_number)
+    {
+        BufferPush(numbers, i);
+        i++;
+    }
+    
+    i = 0;
+    while(i < max_number)
+    {
+        Assert(numbers[i] == i);
+        i++;
+    }
+    
+    BufferFree(numbers);
+}
+
+int main(void)    
 {
     BufferTest();
     LexerTest();
